f0d84a812795dd1826c656a2420e870b850a2830
mithil <mithilsalunkhe@outlook.com>
2025-10-03 12:45:02 -0500

    Changes to test

--- tests/tests.cc
+++ tests/tests.cc
@@ -74,3 +74,10 @@ TEST_CASE("Example: Print Prompt Ledger", "[ex-3]") {
   atm.PrintLedger("./prompt.txt", 12345678, 1234);
   REQUIRE(CompareFiles("./ex-1.txt", "./prompt.txt"));
 }
+
+TEST_CASE("Example: Register duplicate account", "[re-1]") {
+  Atm atm;
+  atm.RegisterAccount(12345678, 1234, "Sam Sepiol", 300.30);
+  REQUIRE_THROWS(atm.RegisterAccount(12345678, 1234, "Sam Sepiol", 300.30));
+  
+}
