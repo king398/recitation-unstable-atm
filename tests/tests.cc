cac4f9dcb556fca5ab658630900ef95cca22f32e
mithil <mithilsalunkhe@outlook.com>
2025-10-03 12:52:51 -0500

    Negative

--- tests/tests.cc
+++ tests/tests.cc
@@ -80,5 +80,11 @@ TEST_CASE("Example: Register duplicate account", "[re-1]") {
   atm.RegisterAccount(12345678, 1234, "Sam Sepiol", 300.30);
   REQUIRE_THROWS(atm.RegisterAccount(12345678, 1234, "Sam Sepiol", 300.30));
   
+
+}
+TEST_CASE("Negative widthdraw", "[withdraw]") {
+  Atm atm;
+  atm.RegisterAccount(12345678, 1234, "Sam Sepiol", 300.30);
+  REQUIRE_THROWS(atm.WithdrawCash(12345678, 1234, -20));
   
 }
